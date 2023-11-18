27a6857560cd5c354bcee7a560150d916e8fa044
Takeshi Abe <t.abe@yamaguchi-u.ac.jp>
2023-11-18 18:28:28 +0900

    2023-11-18

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -39,17 +39,40 @@ example : min a b = min b a := by
     apply min_le_left
 
 example : max a b = max b a := by
-  sorry
+  apply max_comm
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  apply min_assoc
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  have h1 : min a b ≤ a := by
+    apply min_le_left
+  have h2 : min a b ≤ b := by
+    apply min_le_right
+  have h3 : min a b + c ≤ a + c := by
+    apply add_le_add_right h1
+  have h4 : min a b + c ≤ b + c := by
+    apply add_le_add_right h2
+  rw [le_min_iff]
+  exact And.intro h3 h4
+theorem my_aux : min (a + c) (b + c) ≤ min a b + c := by
+  have h1 : min a b = a ∨ min a b = b := by
+    apply min_choice
+  apply Or.elim h1
+  · intro h2
+    rw [h2]
+    apply min_le_left
+  · intro h3
+    rw [h3]
+    apply min_le_right
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  apply aux
+  apply my_aux
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+example : |a| - |b| ≤ |a - b| := calc
+  |a| - |b| = |a - b + b| - |b| := by rw [sub_add, sub_self, sub_zero]
+  _ ≤ |a - b| + |b| - |b| := sub_le_sub_right (abs_add (a - b) b) |b|
+  _ = |a - b| := by rw [← add_sub, sub_self, add_zero]
 end
 
 section
@@ -66,7 +89,17 @@ example : x ∣ x ^ 2 := by
    apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  have h0 : x ∣ y * (x * z) := by
+    apply dvd_mul_of_dvd_right
+    apply dvd_mul_of_dvd_left dvd_rfl
+  have h1 : x ∣ x ^ 2 := by
+    apply dvd_pow dvd_rfl
+    linarith
+  have h2 : x ∣ y * (x * z) + x ^ 2 := dvd_add h0 h1
+  have h3 : x ∣ w ^ 2 := by
+    apply dvd_pow h
+    linarith
+  apply dvd_add h2 h3
 end
 
 section
@@ -77,8 +110,8 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_right n : Nat.lcm n 0 = 0)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
-example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+example : Nat.gcd m n = Nat.gcd n m := gcd_comm m n
+
 end
 
 
--- MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
+++ MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
@@ -34,22 +34,70 @@ variable (x y z : α)
 #check (sup_le : x ≤ z → y ≤ z → x ⊔ y ≤ z)
 
 example : x ⊓ y = y ⊓ x := by
-  sorry
+  apply le_antisymm
+  repeat
+    apply le_inf
+    · apply inf_le_right
+    · apply inf_le_left
+
+theorem my_le_inf_left { a b c : α } : a ≤ b → c ⊓ a ≤ b := by
+  intro h
+  have h1 : c ⊓ a ≤ a := by apply inf_le_right
+  apply le_trans h1 h
+
+theorem my_le_inf_right { a b c : α } : a ≤ b → a ⊓ c ≤ b := by
+  intro h
+  rw [inf_comm]
+  exact my_le_inf_left h
+
+theorem my_le_inf_inf_left { a b c : α } : a ≤ b → c ⊓ a ≤ c ⊓ b := by
+  intro h
+  apply le_inf
+  · apply inf_le_left
+  · exact my_le_inf_left h
+
+theorem my_le_inf_inf_right { a b c : α } : a ≤ b → a ⊓ c ≤ b ⊓ c := by
+  intro h
+  rw [inf_comm]
+  apply le_inf
+  · exact my_le_inf_left h
+  · exact inf_le_left
 
 example : x ⊓ y ⊓ z = x ⊓ (y ⊓ z) := by
-  sorry
+  apply le_antisymm
+  · apply le_inf
+    · exact my_le_inf_right inf_le_left
+    · exact my_le_inf_inf_right inf_le_right
+  · apply le_inf
+    · exact my_le_inf_inf_left inf_le_left
+    · exact my_le_inf_left inf_le_right
 
 example : x ⊔ y = y ⊔ x := by
-  sorry
+  exact sup_comm
 
 example : x ⊔ y ⊔ z = x ⊔ (y ⊔ z) := by
-  sorry
+  exact sup_assoc
+
+theorem my_inf_self { a : α } : a ⊓ a = a := by
+  apply le_antisymm
+  · exact inf_le_left
+  · apply le_inf
+    · exact le_rfl
+    · exact le_rfl
 
 theorem absorb1 : x ⊓ (x ⊔ y) = x := by
-  sorry
+  apply le_antisymm
+  · exact inf_le_left
+  · apply le_inf
+    · exact le_rfl
+    · exact le_sup_left
 
 theorem absorb2 : x ⊔ x ⊓ y = x := by
-  sorry
+  apply le_antisymm
+  · apply sup_le
+    · exact le_rfl
+    · exact inf_le_left
+  · exact le_sup_left
 
 end
 
@@ -67,12 +115,65 @@ section
 variable {α : Type*} [Lattice α]
 variable (a b c : α)
 
+theorem my_le_sup_sup_left { x y z : α } : x ≤ y → z ⊔ x ≤ z ⊔ y := by
+  intro h
+  apply sup_le
+  · exact le_sup_left
+  · exact le_trans h le_sup_right
+
+theorem my_le_sup_sup_right { x y z : α } : x ≤ y → x ⊔ z ≤ y ⊔ z := by
+  intro h
+  rw [sup_comm]
+  apply sup_le
+  · exact le_sup_right
+  · exact le_trans h le_sup_left
+
+theorem my_le_le_inf_inf { a b x y : α } ( h1 : a ≤ b ) ( h2 : x ≤ y ) : a ⊓ x ≤ b ⊓ y := by
+  rw [inf_comm]
+  apply le_inf
+  · exact le_trans inf_le_right h1
+  · exact le_trans inf_le_left h2
+
+theorem my_le_le_sup_sup { a b x y : α } ( h1 : a ≤ b ) ( h2 : x ≤ y ) : a ⊔ x ≤ b ⊔ y := by
+  rw [sup_comm]
+  apply sup_le
+  · exact le_trans h2 le_sup_right
+  · exact le_trans h1 le_sup_left
+
 example (h : ∀ x y z : α, x ⊓ (y ⊔ z) = x ⊓ y ⊔ x ⊓ z) : a ⊔ b ⊓ c = (a ⊔ b) ⊓ (a ⊔ c) := by
-  sorry
+  have h1 : (a ⊔ b) ⊓ a = a := by
+    apply le_antisymm
+    · apply inf_le_right
+    · exact le_inf le_sup_left le_rfl
+  rw [h, h1]
+  apply le_antisymm
+  · apply my_le_sup_sup_left
+    apply my_le_inf_inf_right
+    exact le_sup_right
+  · apply sup_le
+    · exact le_sup_left
+    · rw [inf_comm, h]
+      apply my_le_le_sup_sup
+      · exact inf_le_right
+      · exact le_rfl
 
 example (h : ∀ x y z : α, x ⊔ y ⊓ z = (x ⊔ y) ⊓ (x ⊔ z)) : a ⊓ (b ⊔ c) = a ⊓ b ⊔ a ⊓ c := by
-  sorry
-
+  have h1 : (a ⊓ b) ⊔ a = a := by
+    apply le_antisymm
+    · exact sup_le inf_le_left le_rfl
+    · exact le_sup_right
+  rw [h, h1]
+  apply le_antisymm
+  · apply le_inf
+    · exact inf_le_left
+    · have h2 : a ⊓ b ⊔ c = c ⊔ a ⊓ b := by rw [sup_comm]
+      rw [sup_comm, h2, h]
+      apply my_le_le_inf_inf
+      · exact le_sup_right
+      · exact le_rfl
+  · apply my_le_le_inf_inf
+    · exact le_rfl
+    · exact my_le_sup_sup_right inf_le_right
 end
 
 section
@@ -84,15 +185,37 @@ variable (a b c : R)
 
 #check (mul_nonneg : 0 ≤ a → 0 ≤ b → 0 ≤ a * b)
 
+theorem my_le_zero_le_sub (h: a ≤ b) : 0 ≤ b - a := calc
+  0 = -a + a := by simp
+  _ ≤ -a + b := by apply add_le_add_left h
+  _ = b - a := by apply neg_add_eq_sub
+
 example (h : a ≤ b) : 0 ≤ b - a := by
-  sorry
+  apply my_le_zero_le_sub
+  exact h
+
+theorem my_zero_le_sub_le (h: 0 ≤ b - a) : a ≤ b := calc
+  a = a + 0 := by simp
+  _ ≤ a + (b - a) := by apply add_le_add_left h
+  _ = b := by simp
 
 example (h: 0 ≤ b - a) : a ≤ b := by
-  sorry
+  apply my_zero_le_sub_le
+  exact h
 
 example (h : a ≤ b) (h' : 0 ≤ c) : a * c ≤ b * c := by
-  sorry
-
+  have h1 : 0 ≤ b - a := by
+    apply my_le_zero_le_sub
+    exact h
+  have h2 : 0 ≤ (b - a) * c := by
+    exact mul_nonneg h1 h'
+  have h3 : (b - a) * c = b * c - a * c := by
+    apply sub_mul
+  have h4 : 0 ≤ b * c - a * c := by
+    rw [h3] at h2
+    exact h2
+  apply my_zero_le_sub_le
+  exact h4
 end
 
 section
@@ -104,7 +227,12 @@ variable (x y z : X)
 #check (dist_triangle x y z : dist x z ≤ dist x y + dist y z)
 
 example (x y : X) : 0 ≤ dist x y := by
-  sorry
-
+  have h1 : 0 ≤ dist x y * 2 := calc
+    0 = dist x x := by rw [dist_self]
+    _ ≤ dist x y + dist y x := by apply dist_triangle
+    _ = dist x y + dist x y := by rw [dist_comm]
+    _ = dist x y * 2 := by linarith
+  have h2 : (0 : ℝ) < 2 := by simp
+  apply nonneg_of_mul_nonneg_left h1 h2
 end
 
